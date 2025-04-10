da736b4d6796be73af4bd0c413dc05948bf8c5aa
Samaneh Papoli <faalsamaneh@gmail.com>
2025-04-10 08:11:18 +0200

    vervollständigung

--- script.js
+++ script.js
@@ -41,7 +41,10 @@ let questions = [
   },
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio("audio/succes.mp3");
+let AUDIO_FAIL = new Audio("audio/fail.mp3");
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
@@ -49,38 +52,69 @@ function init() {
 }
 
 function showQuestion() {
-  if (currentQuestion >= questions.length) {
+  if (gameIsOver()) {
+    showEndScreen();
   } else {
-    let question = questions[currentQuestion];
-
-    document.getElementById("question-number").innerHTML = currentQuestion + 1;
-    document.getElementById("questiontext").innerHTML = question["question"];
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];
+    updateProgressBar();
+    updateToNextQuestion();
   }
+}
+function gameIsOver(){
+  return currentQuestion >= questions.length;
+}
+
+function showEndScreen(){
+  document.getElementById("endScreen").style = "";
+  document.getElementById("questionBody").style = "display:none";
+  document.getElementById("amount-of-question").innerHTML = questions.length;
+  document.getElementById("amount-of-right-question").innerHTML =
+    rightQuestions;
+  document.getElementById("header-image").src = "img/trophy.png";
+
+}
+
+function updateProgressBar(){
+  let percent = (currentQuestion + 1) / questions.length;
+  percent = Math.round(percent * 100);
+
+  document.getElementById("progress-bar").innerHTML = `${percent}%`;
+  document.getElementById("progress-bar").style = `width:${percent}%;`;
+}
+
+function updateToNextQuestion(){
+  let question = questions[currentQuestion];
+
+  document.getElementById("question-number").innerHTML = currentQuestion + 1;
+  document.getElementById("questiontext").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
 
 function answer(selection) {
   let question = questions[currentQuestion];
-  console.log("Selected answer is ", selection);
   let selectedQuestionNumber = selection.slice(-1);
-  console.log("selectedQuestionNumber is ", selectedQuestionNumber);
-  console.log("Current question is ", question["right_answer"]);
-
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
-  if (selectedQuestionNumber == question["right_answer"]) {
+  if (rightAnswerSelected(selectedQuestionNumber)) {
     document.getElementById(selection).parentNode.classList.add("bg-success");
+    AUDIO_SUCCESS.play();
+    rightQuestions++;
   } else {
     document.getElementById(selection).parentNode.classList.add("bg-danger");
     document
       .getElementById(idOfRightAnswer)
       .parentNode.classList.add("bg-success");
+      AUDIO_FAIL.play();
   }
   document.getElementById("next-button").disabled = false;
 }
+
+function rightAnswerSelected(selectedQuestionNumber){
+  return selectedQuestionNumber == questions["right_answer"];
+}
+
 function nextQuestion() {
   currentQuestion++;
   document.getElementById("next-button").disabled = true;
@@ -97,4 +131,12 @@ function resetAnswerButtons() {
   document.getElementById("answer_3").parentNode.classList.remove("bg-success");
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
   document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+}
+function restartGame() {
+  document.getElementById("header-image").src = "img/pencil.jpg";
+  document.getElementById("questionBody").style = "";
+  document.getElementById("endScreen").style = "display:none";
+  rightQuestions = 0;
+  currentQuestion = 0;
+  init();
 }
